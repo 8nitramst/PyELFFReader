169e919c8c306d710d405c88610e6f28f963086e
8nitramst <8nitramst>
2017-08-17 23:55:52 +0200

    Comments added

--- PyELFFReader/elffreader.py
+++ PyELFFReader/elffreader.py
@@ -1,11 +1,14 @@
 import shlex, itertools
 
 class ELFFLogreader:
+    """
+    This Class reads an given file in ELFF format, which has a header line for the used fields in the log.
+    The content is crunched to a list of dictionaries, where one dictionary is one log line.
+
+
+    """
     m_file=""
     m_dictLogList = []
-
-    m_dictLogValues=[]
-
 
     def __init__(self,fname):
         self.m_file=fname
@@ -14,7 +17,9 @@ class ELFFLogreader:
             filecontent = reader.readlines()
 
         comments=[]
+        dictLogKeys=None
 
+        # read the header comments and prepare the "#Fields - Header" for parsing to dict-keys
         for i in filecontent:
             if i.startswith("#"):
                 if i.startswith("#Fields: "):
@@ -24,6 +29,7 @@ class ELFFLogreader:
                 else:
                     comments.append(i)
 
+        # remove the comment lines
         for i in comments:
             filecontent.remove(i)
 
@@ -35,10 +41,16 @@ class ELFFLogreader:
         # #Date: 2005-04-27 20:57:09
         # #Fields: date time time-taken c-ip sc-status s-action sc-bytes cs-bytes cs-method cs-uri-scheme cs-host cs-uri-path cs-uri-query cs-username s-hierarchy s-supplier-name rs(Content-Type) cs(User-Agent) sc-filter-result sc-filter-category x-virus-id s-ip s-sitename x-virus-details x-icap-error-code x-icap-error-details
 
+        # crunch the log lines to a dictionary
         for logline in filecontent:
             dictLogValues = shlex.split(logline)
             dictLogLine = dict(itertools.izip(dictLogKeys, dictLogValues))
             self.m_dictLogList.append(dictLogLine)
 
     def getLogs(self):
+        """ Get the content of the logfile as a list of dict
+
+
+        :return: list(dict[]) List of Dictionary - where key is the ELFF-Header
+        """
         return self.m_dictLogList
